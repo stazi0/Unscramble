38b27f8913109ef314c32c45b491bc6211e52a9f
Andrij Demchuk <stazi@ukr.net>
2023-11-21 20:44:00 +0200

    ViewModel_2

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,14 +4,16 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    init {
-        Log.d("GameFragment", "GameViewModel created!")
-    }
+
     private var score = 0
     private var currentWordCount = 0
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
 
     override fun onCleared() {
         super.onCleared()
