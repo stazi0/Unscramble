0a3cee1e34a6a84b22ff52fb4f66551797b8ff4a
Andrij Demchuk <stazi@ukr.net>
2023-11-21 21:31:26 +0200

    submit

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    private var score = 0
+    private var _score = 0
     val score: Int
         get() = _score
     private var currentWordCount = 0
@@ -46,5 +46,17 @@ class GameViewModel : ViewModel() {
             getNextWord()
             true
         } else false
+    }
+
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
     }
 }
